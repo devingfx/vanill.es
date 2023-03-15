5c5079f5eb350b85d5e150138cca1a393cabe6b6
DiG <dig@p2p.legal>
2023-03-15 04:32:17 +0100

    [YAML] add .stringify

--- String/YAML.js
+++ String/YAML.js
@@ -2,5 +2,6 @@ import merge from "./merge.js"
 
 const YAML = (...a)=> jsyaml.load( merge(...a) )
 YAML.import = ()=> import('https://unpkg.com/js-yaml@4.1.0/dist/js-yaml.min.js')
+YAML.stringify = obj=> jsyaml.dump(obj)
 
 export default YAML
