6544bbf43e43cc540ca4c2b56c3c8bff1da8d296
DiG <dig@p2p.legal>
2023-03-15 04:36:17 +0100

    [DOM] CSS can have placeHolders

--- DOM/CSS.js
+++ DOM/CSS.js
@@ -1,5 +1,5 @@
-
+import merge from "../String/merge.js"
 /**
  * CSS`selector { prop: val; }`
  */
-export default css=> { let s = new CSSStyleSheet; s.replaceSync(css); return s }
+export default (...css)=> { let s = new CSSStyleSheet; s.replaceSync(merge(...css)); return s }
